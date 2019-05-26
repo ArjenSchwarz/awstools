b7d738ef0cba7d2656b733a784a3de0f61a13b35
Arjen Schwarz <developer@arjen.eu>
2019-05-26 14:08:56 +1000

    Disable WIP sg command

--- cmd/listrules.go
+++ cmd/listrules.go
@@ -1,94 +1,94 @@
 package cmd
 
-import (
-	"fmt"
-	"strconv"
+// (import (
+// 	"fmt"
+// 	"strconv"
 
-	"github.com/ArjenSchwarz/awstools/helpers"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/spf13/cobra"
-)
+// 	"github.com/ArjenSchwarz/awstools/helpers"
+// 	"github.com/aws/aws-sdk-go/aws"
+// 	"github.com/aws/aws-sdk-go/service/ec2"
+// 	"github.com/spf13/cobra"
+// )
 
-// listrulesCmd represents the listrules command
-var listrulesCmd = &cobra.Command{
-	Use:   "listrules",
-	Short: "List the rules for the security groups",
-	Long:  `Lists all the rules for the security groups`,
-	Run:   listRules,
-}
+// // listrulesCmd represents the listrules command
+// var listrulesCmd = &cobra.Command{
+// 	Use:   "listrules",
+// 	Short: "List the rules for the security groups",
+// 	Long:  `Lists all the rules for the security groups`,
+// 	Run:   listRules,
+// }
 
-func init() {
-	sgCmd.AddCommand(listrulesCmd)
-}
+// func init() {
+// 	sgCmd.AddCommand(listrulesCmd)
+// }
 
-func listRules(cmd *cobra.Command, args []string) {
-	svc := helpers.Ec2Session()
-	params := &ec2.DescribeSecurityGroupsInput{}
-	filters := make([]*ec2.Filter, 1)
-	if *vpc != "" {
-		values := make([]*string, 1)
-		values = append(values, vpc)
-		filter := ec2.Filter{
-			Name:   aws.String("vpc-id"),
-			Values: values,
-		}
-		filters = append(filters, &filter)
-	}
-	params.SetFilters(filters)
-	// if *groupname != "" {
-	// 	groupnames := make([]*string, 1)
-	// 	groupnames = append(groupnames, groupname)
-	// 	params.GroupNames = groupnames
-	// }
-	resp, err := svc.DescribeSecurityGroups(params)
-	if err != nil {
-		panic(err)
-	}
-	rules := make([]sgRule, len(resp.SecurityGroups))
-	for _, group := range resp.SecurityGroups {
-		for _, permission := range group.IpPermissions {
-			for _, ip := range permission.IpRanges {
-				if aws.StringValue(ip.CidrIp) == "" {
-					fmt.Println(aws.StringValue(group.GroupName))
-				}
-				rule := sgRule{
-					SecurityGroupName: aws.StringValue(group.GroupName),
-					FromPort:          aws.Int64Value(permission.FromPort),
-					ToPort:            aws.Int64Value(permission.ToPort),
-					Source:            aws.StringValue(ip.CidrIp),
-				}
-				rules = append(rules, rule)
-			}
-		}
-	}
-	keys := []string{"SecurityGroup", "Ports", "Source"}
-	output := helpers.OutputArray{Keys: keys}
-	for _, resource := range rules {
-		if resource.SecurityGroupName != "" {
+// func listRules(cmd *cobra.Command, args []string) {
+// 	svc := helpers.Ec2Session()
+// 	params := &ec2.DescribeSecurityGroupsInput{}
+// 	filters := make([]*ec2.Filter, 1)
+// 	if *vpc != "" {
+// 		values := make([]*string, 1)
+// 		values = append(values, vpc)
+// 		filter := ec2.Filter{
+// 			Name:   aws.String("vpc-id"),
+// 			Values: values,
+// 		}
+// 		filters = append(filters, &filter)
+// 	}
+// 	params.SetFilters(filters)
+// 	// if *groupname != "" {
+// 	// 	groupnames := make([]*string, 1)
+// 	// 	groupnames = append(groupnames, groupname)
+// 	// 	params.GroupNames = groupnames
+// 	// }
+// 	resp, err := svc.DescribeSecurityGroups(params)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	rules := make([]sgRule, len(resp.SecurityGroups))
+// 	for _, group := range resp.SecurityGroups {
+// 		for _, permission := range group.IpPermissions {
+// 			for _, ip := range permission.IpRanges {
+// 				if aws.StringValue(ip.CidrIp) == "" {
+// 					fmt.Println(aws.StringValue(group.GroupName))
+// 				}
+// 				rule := sgRule{
+// 					SecurityGroupName: aws.StringValue(group.GroupName),
+// 					FromPort:          aws.Int64Value(permission.FromPort),
+// 					ToPort:            aws.Int64Value(permission.ToPort),
+// 					Source:            aws.StringValue(ip.CidrIp),
+// 				}
+// 				rules = append(rules, rule)
+// 			}
+// 		}
+// 	}
+// 	keys := []string{"SecurityGroup", "Ports", "Source"}
+// 	output := helpers.OutputArray{Keys: keys}
+// 	for _, resource := range rules {
+// 		if resource.SecurityGroupName != "" {
 
-			content := make(map[string]string)
-			content["SecurityGroup"] = resource.SecurityGroupName
-			if resource.FromPort == resource.ToPort {
-				if resource.FromPort == 0 {
-					content["Ports"] = "ALL"
-				} else {
-					content["Ports"] = strconv.FormatInt(resource.FromPort, 10)
-				}
-			} else {
-				content["Ports"] = strconv.FormatInt(resource.FromPort, 10) + " - " + strconv.FormatInt(resource.ToPort, 10)
-			}
-			content["Source"] = resource.Source
-			holder := helpers.OutputHolder{Contents: content}
-			output.AddHolder(holder)
-		}
-	}
-	output.Write(*settings)
-}
+// 			content := make(map[string]string)
+// 			content["SecurityGroup"] = resource.SecurityGroupName
+// 			if resource.FromPort == resource.ToPort {
+// 				if resource.FromPort == 0 {
+// 					content["Ports"] = "ALL"
+// 				} else {
+// 					content["Ports"] = strconv.FormatInt(resource.FromPort, 10)
+// 				}
+// 			} else {
+// 				content["Ports"] = strconv.FormatInt(resource.FromPort, 10) + " - " + strconv.FormatInt(resource.ToPort, 10)
+// 			}
+// 			content["Source"] = resource.Source
+// 			holder := helpers.OutputHolder{Contents: content}
+// 			output.AddHolder(holder)
+// 		}
+// 	}
+// 	output.Write(*settings)
+// }
 
-type sgRule struct {
-	SecurityGroupName string
-	FromPort          int64
-	ToPort            int64
-	Source            string
-}
+// type sgRule struct {
+// 	SecurityGroupName string
+// 	FromPort          int64
+// 	ToPort            int64
+// 	Source            string
+// })
--- cmd/sg.go
+++ cmd/sg.go
@@ -1,23 +1,23 @@
 package cmd
 
-import (
-	"github.com/spf13/cobra"
-)
+// import (
+// 	"github.com/spf13/cobra"
+// )
 
-// sgCmd represents the sg command
-var sgCmd = &cobra.Command{
-	Use:   "sg",
-	Short: "Security Group commands",
-	Long:  `Various security group related tasks`,
-}
+// // sgCmd represents the sg command
+// var sgCmd = &cobra.Command{
+// 	Use:   "sg",
+// 	Short: "Security Group commands",
+// 	Long:  `Various security group related tasks`,
+// }
 
-var groupname *string
-var tag *string
-var vpc *string
+// var groupname *string
+// var tag *string
+// var vpc *string
 
-func init() {
-	RootCmd.AddCommand(sgCmd)
-	groupname = sgCmd.PersistentFlags().StringP("groupname", "g", "", "The name of the securitygroup")
-	tag = sgCmd.PersistentFlags().StringP("tag", "t", "", "key:value pair of tag value")
-	vpc = sgCmd.PersistentFlags().String("vpc", "", "VPC Id")
-}
+// func init() {
+// 	RootCmd.AddCommand(sgCmd)
+// 	groupname = sgCmd.PersistentFlags().StringP("groupname", "g", "", "The name of the securitygroup")
+// 	tag = sgCmd.PersistentFlags().StringP("tag", "t", "", "key:value pair of tag value")
+// 	vpc = sgCmd.PersistentFlags().String("vpc", "", "VPC Id")
+// }
