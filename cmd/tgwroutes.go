44bcbcdcf1862800bd40033ed126f2608030eef9
Arjen Schwarz <arjen.schwarz@digio.com.au>
2020-02-22 17:05:09 +1100

    Update tgw routes to tgw routetables and add filter
    You can now filter the route tables by VPC, route table, or transit
    gateway

--- cmd/tgwroutes.go
+++ cmd/tgwroutes.go
@@ -10,25 +10,34 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// tgwroutesCmd represents the tgwroutes command
-var tgwroutesCmd = &cobra.Command{
-	Use:   "routes",
+// tgwroutetablesCmd represents the tgwroutes command
+var tgwroutetablesCmd = &cobra.Command{
+	Use:   "routetables",
 	Short: "Get an overview of connections between Transit Gateway Route Tables and attached resources",
-	Long: `This is currently limited to showing VPC attachments only, but that will be fixed soon.
+	Long: `Get an overview of connections between Transit Gateway Route Tables and attached resources
+	This is currently limited to showing VPC attachments only, but that will be fixed soon.
+
+	Using the --resource-id (-r) flag, you can limit the output to the provided resource.
+	For a route table that means all the VPCs it connects to,
+	while for a VPC that means all the route tables it connects
+	to and through them what other VPCs can reach it or it can reach.
 
 	Supports a Draw.io output`,
 	Run: tgwroutes,
 }
 
+var tgwresourceid string
+
 func init() {
-	tgwCmd.AddCommand(tgwroutesCmd)
+	tgwCmd.AddCommand(tgwroutetablesCmd)
+	tgwroutetablesCmd.Flags().StringVarP(&tgwresourceid, "resource-id", "r", "", "The id of the resource you want to limit to")
 }
 
 func tgwroutes(cmd *cobra.Command, args []string) {
 	resultTitle := "Overview of all routes"
 	svc := helpers.Ec2Session()
 	gateways := helpers.GetAllTransitGateways(svc)
-	keys := []string{"ID", "Name", "DestinationVPCs", "TargetGateway"}
+	keys := []string{"ID", "Name", "Destinations", "TargetGateway"}
 	if settings.IsDrawIO() {
 		keys = append(keys, "Image")
 	}
@@ -38,42 +47,29 @@ func tgwroutes(cmd *cobra.Command, args []string) {
 		output.DrawIOHeader = createTgwRoutesDrawIOHeader()
 	case "dot":
 		dotcolumns := config.DotColumns{
-			From: "DestinationVPCs",
+			From: "Destinations",
 			To:   "ID",
 		}
 		settings.DotColumns = &dotcolumns
 	}
-	vpcs := make(map[string]string)
-	tgwrts := make(map[string][]string)
-	for _, gateway := range gateways {
-		for _, routetable := range gateway.RouteTables {
-			tgwrts[routetable.ID] = []string{}
-			for _, route := range routetable.Routes {
-				tgwrts[routetable.ID] = append(tgwrts[routetable.ID], route.Attachment.ResourceID)
-				if _, ok := vpcs[route.Attachment.ResourceID]; !ok {
-					vpcs[route.Attachment.ResourceID] = ""
-				}
-			}
-			for _, sourceattachment := range routetable.SourceAttachments {
-				vpcs[sourceattachment.ResourceID] = routetable.ID
-			}
-		}
-	}
+
+	attachedresources, tgwrts := filterGateway(gateways)
+
 	for rt, connectedvpcs := range tgwrts {
 		content := make(map[string]string)
 		content["ID"] = rt
 		content["Name"] = getName(rt)
-		content["DestinationVPCs"] = strings.Join(connectedvpcs, ",")
+		content["Destinations"] = strings.Join(connectedvpcs, ",")
 		if settings.IsDrawIO() {
 			content["Image"] = drawio.ShapeAWSRoute53RouteTable
 		}
 		holder := helpers.OutputHolder{Contents: content}
 		output.AddHolder(holder)
 	}
-	for vpcid, tgw := range vpcs {
+	for resourceid, tgw := range attachedresources {
 		content := make(map[string]string)
-		content["ID"] = vpcid
-		content["Name"] = getName(vpcid)
+		content["ID"] = resourceid
+		content["Name"] = getName(resourceid)
 		content["TargetGateway"] = tgw
 		if settings.IsDrawIO() {
 			content["Image"] = drawio.ShapeAWSVPC
@@ -84,11 +80,68 @@ func tgwroutes(cmd *cobra.Command, args []string) {
 	output.Write(*settings)
 }
 
+func filterGateway(gateways []helpers.TransitGateway) (map[string]string, map[string][]string) {
+	limitertype := ""
+	if tgwresourceid != "" {
+		if tgwresourceid[0:3] == "vpc" { // VPC
+			limitertype = "vpc"
+		} else if tgwresourceid[0:7] == "tgw-rtb" { // Route Table
+			limitertype = "tgw-rtb"
+		} else if tgwresourceid[0:3] == "tgw" { // Transit Gateway
+			limitertype = "tgw"
+		}
+	}
+	attachedresources := make(map[string]string)
+	tgwrts := make(map[string][]string)
+
+	for _, gateway := range gateways {
+		// only add relevant gateway if filtered by gateway
+		if limitertype == "tgw" && gateway.ID != tgwresourceid {
+			continue
+		}
+		for _, routetable := range gateway.RouteTables {
+			// only add relevant route tables if filtered by route table
+			if limitertype == "tgw-rtb" && routetable.ID != tgwresourceid {
+				continue
+			}
+			tgwrts[routetable.ID] = []string{}
+			for _, route := range routetable.Routes {
+				tgwrts[routetable.ID] = append(tgwrts[routetable.ID], route.Attachment.ResourceID)
+				if _, ok := attachedresources[route.Attachment.ResourceID]; !ok {
+					attachedresources[route.Attachment.ResourceID] = ""
+				}
+			}
+			for _, sourceattachment := range routetable.SourceAttachments {
+				attachedresources[sourceattachment.ResourceID] = routetable.ID
+			}
+		}
+	}
+	// For VPC pass over everything and remove what's not relevant
+	if limitertype == "vpc" {
+		attachedtgwrts := []string{}
+		for tgwid, destinationvpcs := range tgwrts {
+			if !contains(destinationvpcs, tgwresourceid) && tgwid != attachedresources[tgwresourceid] {
+				delete(tgwrts, tgwid)
+			}
+			if contains(destinationvpcs, tgwresourceid) {
+				tgwrts[tgwid] = []string{tgwresourceid}
+				attachedtgwrts = append(attachedtgwrts, tgwid)
+			}
+		}
+		for resourceid, tgwrt := range attachedresources {
+			if resourceid != tgwresourceid && !contains(attachedtgwrts, tgwrt) {
+				delete(attachedresources, resourceid)
+			}
+		}
+	}
+	return attachedresources, tgwrts
+}
+
 func createTgwRoutesDrawIOHeader() drawio.Header {
 	drawioheader := drawio.NewHeader("%Name%", "%Image%", "Image")
 	drawioheader.SetHeightAndWidth("78", "78")
 	connection := drawio.NewConnection()
-	connection.From = "DestinationVPCs"
+	connection.From = "Destinations"
 	connection.To = "ID"
 	connection.Invert = false
 	connection.Label = "Outbound"
